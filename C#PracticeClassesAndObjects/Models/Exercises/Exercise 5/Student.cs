3122a9327c157c2164fe75084c8bd409c08d6205
Kirill Bravin <kshaadoo1991@gmail.com>
2024-07-24 10:50:37 +0300

    Completed Exercises 1-5

---
+++ C#PracticeClassesAndObjects/Models/Exercises/Exercise 5/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_PracticeClassesAndObjects.Models.Exercises.Exercise_5
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Grade}";
+        }
+    }
+
+    public class CallStudent
+    {
+        Student[] students;
+
+        public CallStudent()
+        {
+            students = new Student[2];
+
+            students[0] = new Student("Andrew", 9);
+            students[1] = new Student("Tom", 4);
+
+        }
+        public void CheckGrade()
+        {
+            for (int i = 0; i < students.Length; i++) 
+            {
+                if (students[i].Grade >= 5)
+                {
+                    Console.WriteLine($"{students[i].Name} has passed with grade: {students[i].Grade}");
+                } else Console.WriteLine($"{students[i].Name} didn't pass with grade: {students[i].Grade}");
+            }
+        }
+    }
+}
---
+++ C#PracticeClassesAndObjects/Models/Exercises/Exercise1/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_PracticeClassesAndObjects.Models.Exercises.Exercise1
+{
+    public class Car
+    {
+        public string Mark { get; set; }
+        public int Year { get; set; }
+
+        public Car (string mark, int year)
+        {
+            Mark = mark;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Mark} {Year}";
+        }
+    }
+
+
+    public class CallAutos
+    {
+        Car car1 = new Car("BMW", 2002);
+        Car car2 = new Car("Audi", 2016);
+
+        Car[] cars;
+
+        public CallAutos()
+        {
+            cars = new Car[2];
+            cars[0] = car1;
+            cars[1] = car2;
+        }
+
+        public void ShowAutos()
+        {
+            foreach(Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
