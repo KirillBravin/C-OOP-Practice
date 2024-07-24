08ac1ec3124f2677b117534ee00c785e884737a0
Kirill Bravin <kshaadoo1991@gmail.com>
2024-07-24 14:31:48 +0300

    First exercise finished

---
+++ C#PracticeClassesAndObjects/Models/Lesson6/Exercises with lists/Student.cs
@@ -0,0 +1,71 @@
+﻿using C_PracticeClassesAndObjects.Models.Exercises.Exercise_5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_PracticeClassesAndObjects.Models.Lesson6.Exercises_with_lists
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Grade}";
+        }
+    }
+
+    public class StudentList
+    {
+        public List<Student> Students { get; set; }
+
+        public StudentList()
+        {
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student student) 
+        {
+            Students.Add(student);
+        }
+
+        public void ShowStudents()
+        {
+            foreach (Student student in Students) 
+            {
+                if (student.Grade < 50)
+                {
+                    Console.WriteLine($"{student.Name} with grade: {student.Grade}");
+                }
+            }
+        }
+    }
+
+    public class CallStudentList()
+    {
+        StudentList studentList = new StudentList();
+
+        public void AddingStudents()
+        {
+            studentList.AddStudent(new Student("LaraCroft", 78));
+            studentList.AddStudent(new Student("TimBert", 22));
+            studentList.AddStudent(new Student("SamuelJackson", 99));
+            studentList.AddStudent(new Student("IndianaJones", 55));
+            studentList.AddStudent(new Student("LukeSkywalker", 14));
+        }
+
+        public void ShowStudentsWithBadGrades()
+        {
+            studentList.ShowStudents();
+        }
+    }
+}
---
+++ C#PracticeClassesAndObjects/Models/Lesson6/Extra Exercises/Exercise 3/Pyramid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_PracticeClassesAndObjects.Models.Lesson6.Extra_Exercises.Exercise_3
+{
+    // In progress..
+    public class Pyramid
+    {
+        public void ConstructingPyramid()
+        {
+            Console.WriteLine("Please enter a number: ");
+            int input = int.Parse(Console.ReadLine());
+            int area = input - 1;
+
+            for (int i = 0; i < input; i++)
+            {
+                for (double j = 0; j < input*2-1; j++)
+                {
+                    //Console.Write("*");
+                    //if ()
+                    //{
+                    //    Console.Write("*");
+                    //}
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
