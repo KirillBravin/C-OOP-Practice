3facb816b2ee0000a82f4c0fc6f7f4fbc20ac40e
Kirill Bravin <kshaadoo1991@gmail.com>
2024-07-29 08:43:03 +0300

    Minor bug fix

--- C#PracticeClassesAndObjects/Lessons/Lesson 8/Exercise 4/Models/NumberPrinter.cs
+++ C#PracticeClassesAndObjects/Lessons/Lesson 8/Exercise 4/Models/NumberPrinter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C_PracticeClassesAndObjects.Lessons.Lesson_8.Exercise_4.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace C_PracticeClassesAndObjects.Lessons.Lesson_8.Exercise_4.Service
 {
-    public class NumberPrinter
+    public class NumberPrinter : INumberPrinter
     {
         public void PrintingNumbers()
         {
